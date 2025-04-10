c024f8c9438fa57c536acbf4f9eef3563552cd5f
zhamiliabegalieva <jbegalieva@gmail.com>
2025-04-09 18:40:11 -0700

    updated. test failed
    removed the 29th row but still have fails.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,10 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-
 public class LoginTests extends BaseTest {
     @Test(enabled = false, description = "Test has been marked as skipped due to am issue in Jira-124111")
-//   @Test
+
     public void loginValidEmailPassword() {
 
         navigateToPage();
@@ -15,11 +14,10 @@ public class LoginTests extends BaseTest {
         providePassword("12345678");
         clickSubmit();
 
-       Assert.assertEquals(driver.getCurrentUrl(), url); // asserting that website url stays the same
-       WebElement avatarIcon = driver.findElement(By.xpath("//img[@class='avatar']"));
-       Assert.assertTrue(avatarIcon.isDisplayed());
-
-      }
+        Assert.assertEquals(driver.getCurrentUrl(), url); // asserting that website url stays the same
+        WebElement avatarIcon = driver.findElement(By.xpath("//img[@class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+    }
 
     @Test(priority = 1)
     public void loginInvalidEmailValidPassword() {
